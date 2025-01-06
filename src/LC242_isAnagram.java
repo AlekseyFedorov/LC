a38c2cd588c4105ae3ffc3c857b280c68d1203bf
Aleksey Fedorov <Aleksey.G.Fedorov@gmail.com>
2025-01-06 14:33:52 +0300

    LC242_isAnagram added

---
+++ src/LC242_isAnagram.java
@@ -0,0 +1,11 @@
+public class LC242_isAnagram {
+
+    public boolean isAnagram(String s1, String s2) {
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(new LC242_isAnagram().isAnagram("friend", "Finder"));
+    }
+}
