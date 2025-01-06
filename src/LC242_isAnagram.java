ee3235a8cbfde95fbf72cdbf4d4780473fc91f4c
Aleksey Fedorov <Aleksey.G.Fedorov@gmail.com>
2025-01-06 15:02:44 +0300

    LC242_isAnagram added

--- src/LC242_isAnagram.java
+++ src/LC242_isAnagram.java
@@ -1,6 +1,15 @@
 public class LC242_isAnagram {
 
     public boolean isAnagram(String s1, String s2) {
+        if (s1.length() != s2.length())
+            return false;
+
+        int[] freq = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            freq[s1.charAt(i) - 'a']++;
+            freq[s2.charAt(i) - 'a']--;
+        }
+
         return true;
     }
 
